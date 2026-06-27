8c0e6842ef6df4d1c53176ff2506c0b4bdc34140
Fernanda-Marinho <fmarinho1918@gmail.com>
2026-06-26 21:50:30 -0300

    feat: add migration in app file

--- api/app.py
+++ api/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_migrate import Migrate
+from models import db, Task, Category 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:pass@db:5432/tasks_db'
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
